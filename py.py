cc918408b8e9843d00e6deea9841d993af4b38ed
Nandini <187662706+goyal-coder@users.noreply.github.com>
2025-02-27 21:13:14 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,12 @@
+# Madlibs game
+
+word1=input("Enter a place:")
+word2=input("Enter name of human/animal :")
+word3=input("Enter a place :")
+word4=input("Enter food :")
+word5=input("Enter a scene (keep guessing what a scene is HAHA) :")
+word6=input("Enter a feeling :")
+word7=input("Enter a feeling again:")
+word8=input("Enter a scene again :")
+
+print(f"Today i went to {word1} and met {word2}. After meeting we went to {word3} where we ate {word4}. while eating i saw {word5} which felt {word6}. After watching this whole {word7} scene. I woke up and saw {word8}")
